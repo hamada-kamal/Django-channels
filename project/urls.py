3ca3140921eb5bec3faf24b878baf9c03e3ad814
hamada-kamal <hamadakamal819gmail.com>
2021-04-28 13:06:53 +0200

    add chat template & load messages

--- project/urls.py
+++ project/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include
-from django.urls import path
+from django.conf.urls import url
 from django.contrib import admin
-
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    path('chat/', include('chat.urls')),
     path('admin/', admin.site.urls),
+    path('chat/', include('chat.urls', namespace='chat')),
 ]
+urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
